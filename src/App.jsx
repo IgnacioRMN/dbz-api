a24374003081a637e0cc309f3b3fb650389af89b
José Ignacio Ramón <joseignacioramon24@gmail.com>
2025-04-11 01:17:10 -0300

    Se mejoró el maquetado, tanto la card como el container

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Frase from "./components/Frase";
 function App() {
   return (
     <>
-    <Container className="text-center my-4 d-flex flex-column align-items-center">
-          <Frase ></Frase>
+      <Container className="text-center my-4 d-flex flex-column align-items-center">
+        <Frase></Frase>
 
-        <Button variant="warning" className="mt-3" >
+        <Button variant="warning" className="mt-3">
           Obtener frase
         </Button>
       </Container>
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,43 +1,59 @@
 import React from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
 
-const Frase = () => {
-  // https://web.dragonball-api.com/
+function Frase() {
   return (
-    <Card className="my-4" data-bs-theme="dark">
+    <Card
+      className="my-1 shadow-lg rounded-4 overflow-hidden bg-dark text-white"
+      style={{ maxWidth: "380px" }}
+      data-bs-theme="dark"
+    >
       <Card.Img
-        className="imgCard"
+        className="p-2"
         variant="top"
         src="https://dragonball-api.com/characters/goku_normal.webp"
+        style={{ height: "250px", objectFit: "contain" }}
       />
-      <Card.Body className="text-start">
-        <Card.Title className="fs-3 ">Goku</Card.Title>
-        <p>
-          <b>Raza:</b> Saiyan
+      <Card.Body className="p-4 text-center">
+        <Card.Title className="fs-3 fw-bold text-primary text-uppercase">
+          Goku
+        </Card.Title>
+
+        <Row className="mb-3 justify-content-center gap-2">
+          <Col
+            xs={6}
+            className="w-auto badge text-light-emphasis bg-light-subtle border border-light-subtle rounded-pill"
+          >
+            <strong>Raza:</strong> Saiyan
+          </Col>
+          <Col
+            xs={6}
+            className="w-auto  badge text-light-emphasis bg-light-subtle border border-light-subtle rounded-pill"
+          >
+            <strong>Ki:</strong> 60.000.000
+          </Col>
+        </Row>
+
+        <p
+          className="text-secondary"
+          style={{
+            maxHeight: "6em",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          El protagonista de la serie, conocido por su gran poder y personalidad
+          amigable. Fue enviado a la Tierra para sobrevivir a la destrucción de
+          su planeta. Con el tiempo, se convertiría en el defensor del Universo
+          7, liderando a los Guerreros Z.
         </p>
-        <p>
-          <b>Ki:</b> 60.000.000
-        </p>
-        <p>
-          <b>Descripción:</b> El protagonista de la serie, conocido por su gran
-          poder y personalidad amigable. Originalmente enviado a la Tierra como
-          un infante volador con la misión de conquistarla. Sin embargo, el caer
-          por un barranco le proporcionó un brutal golpe que si bien casi lo
-          mata, este alteró su memoria y anuló todos los instintos violentos de
-          su especie, lo que lo hizo crecer con un corazón puro y bondadoso,
-          pero conservando todos los poderes de su raza. No obstante, en la
-          nueva continuidad de Dragon Ball se establece que él fue enviado por
-          sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la
-          destrucción de su planeta por parte de Freeza. Más tarde, Kakarot,
-          ahora conocido como Son Goku, se convertiría en el príncipe consorte
-          del monte Fry-pan y líder de los Guerreros Z, así como el mayor
-          defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo
-          de la destrucción en innumerables ocasiones, a pesar de no
-          considerarse a sí mismo como un héroe o salvador.
-        </p>
+
+        <Button variant="outline-light" size="sm">
+          Ver más
+        </Button>
       </Card.Body>
     </Card>
   );
-};
+}
 
 export default Frase;
