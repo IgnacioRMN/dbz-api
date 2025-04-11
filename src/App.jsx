8f3903c5bad0c14e5d307c4aaed1a8fa6b816e2f
José Ignacio Ramón <joseignacioramon24@gmail.com>
2025-04-11 01:43:30 -0300

    Se empezó a trabajar con la API

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
   return (
     <>
       <Container className="text-center my-4 d-flex flex-column align-items-center">
         <Frase></Frase>
 
         <Button variant="warning" className="mt-3">
-          Obtener frase
+          Obtener Personaje
         </Button>
       </Container>
     </>
